93d55576d27647bee21115c0326df9df8d70a6bc
Nat3y <khalednatey85@gmail.com>
2024-08-28 15:20:23 +0300

    -initial commit

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,4 @@
 abstract class AppRoutes {
-  static const String splash = '/';
   static const String login = '/login';
   static const String register = '/register';
   static const String home = '/home';
