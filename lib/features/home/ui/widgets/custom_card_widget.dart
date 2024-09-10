f991ba94807b7c72c9f510eaaa88b35e078f26a3
Nat3y <khalednatey85@gmail.com>
2024-09-10 12:32:18 +0300

    -added chat room screen. -finished chat room screen.

--- lib/features/home/ui/widgets/custom_card_widget.dart
+++ lib/features/home/ui/widgets/custom_card_widget.dart
@@ -11,44 +11,48 @@ class CustomCardWidget extends StatelessWidget {
   final String description;
   final DateTime currentTime;
   final String imagePath;
-  const CustomCardWidget({super.key, required this.title, required this.description, required this.currentTime, required this.imagePath});
+  final Function()? onTap;
+  const CustomCardWidget({super.key, required this.title, required this.description, required this.currentTime, required this.imagePath, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     String formattedDate = DateFormat("yyyy-MM-dd").format(currentTime); // Display current time
-    return Card(
-      margin: EdgeInsets.all(8.r),
-      elevation: 10,
-      color: AppColors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 120.w,
-            height: 115.h,
-            child: ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(16.r)),
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: EdgeInsets.all(8.r),
+        elevation: 10,
+        color: AppColors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 120.w,
+              height: 115.h,
+              child: ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(16.r)),
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          verticalSpace(10),
-          // Text below the image
-          Text(
-            title,
-            style: AppTheme.font14BlackRegular,
-            textAlign: TextAlign.center,
-          ),
-          verticalSpace(5),
-          // Add dynamically generated time below the title
-          Text(
-            formattedDate,
-            style: AppTheme.font18BlackRegular,
-            textAlign: TextAlign.center,
-          ),
-        ],
+            verticalSpace(10),
+            // Text below the image
+            Text(
+              title,
+              style: AppTheme.font14BlackRegular,
+              textAlign: TextAlign.center,
+            ),
+            verticalSpace(5),
+            // Add dynamically generated time below the title
+            Text(
+              formattedDate,
+              style: AppTheme.font18BlackRegular,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
